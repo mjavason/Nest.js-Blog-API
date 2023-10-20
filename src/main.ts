ce7e3915772cbbdae6ad33fc6565d4fe213fc199
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2023-10-20 20:05:56 +0100

    Initial setup

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('This is a simple blog api built with nest.js')
+    .setVersion('1.0')
+    .addTag('Blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(5000);
+}
+bootstrap();
